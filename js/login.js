009e4fb918deed03e51d470f737dcf24717b41d7
carlopenaaponte <programadorcarlo@hotmail.com>
2023-08-15 21:00:57 -0300

    Desafiate

--- js/login.js
+++ js/login.js
@@ -1,17 +1,47 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const form = document.querySelector("form");
+  
+//     form.addEventListener("submit", function(event) {
+//       event.preventDefault();
+  
+//       const username = document.getElementById("username").value;
+//       const password = document.getElementById("password").value;
+  
+//       if (username.trim() === "" || password.trim() === "") {
+//         alert("Por favor, completa todos los campos.");
+//       } else {
+//         window.location.href = "index.html"; 
+//       }
+//     });
+//   });
+  
+
+
+ // login.js
+
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-  
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const username = document.getElementById("username").value;
-      const password = document.getElementById("password").value;
-  
-      if (username.trim() === "" || password.trim() === "") {
-        alert("Por favor, completa todos los campos.");
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username.trim() === "" || password.trim() === "") {
+      alert("Por favor, completa todos los campos.");
+    } else {
+      // Simulación de autenticación exitosa
+      const autenticacionExitosa = true;
+      
+      if (autenticacionExitosa) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("isLoggedIn", true);
+
+        window.location.href = "index.html"; // Redirige a la página principal después del inicio de sesión
       } else {
-        window.location.href = "index.html"; 
+        alert("Credenciales incorrectas. Por favor, intenta de nuevo.");
       }
-    });
+    }
   });
-  
+});
