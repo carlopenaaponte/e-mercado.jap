fd7d3fb12febe7b9276a8441a4e02dcd27ebb610
carlopenaaponte <programadorcarlo@hotmail.com>
2023-09-29 22:14:51 -0300

    Entrega 4 - v3

--- js/product-info.js
+++ js/product-info.js
@@ -26,59 +26,41 @@ function ObtenerInfo(ID) {
 
       const imagenes = document.getElementById("imagenes");
       imagenes.innerHTML = `<br>${mostrarImagenes(resultado)}<br>`;
+
+      const productosRelacionados = document.getElementById("productosRelacionados");
+      productosRelacionados.innerHTML = `${mostrarProductoRelacionado(resultado)}`;
     })
     .catch((error) => console.error("Ocurrió un error:", error));
 }
 
-// function mostrarImagenes(producto) {
-//   //Se crea un funcion para mostrar las imagenes en un contenedor
-//   let contenedorImagenes = "";
-//   contenedorImagenes += '<div class="row">';
-//   producto.images.forEach((element) => {
-//     contenedorImagenes += `
-//     <div class="col-md-3">
-//         <img class="img-thumbnail" src="${element}" alt="Imagen">
-//         </div>
-//     `;
-//   });
-//   contenedorImagenes += "</div>";
-//   return contenedorImagenes;
-// }
+function setProductID(id){
+  localStorage.setItem("productID", id );
+  window.location = "product-info.html"
+}
 
-// function mostrarImagenes(producto) {
-//   let contenedorImagenes = "";
-//   contenedorImagenes += `
-//     <div id="carouselControls" class="carousel slide" data-bs-ride="carousel">
-//       <div class="carousel-inner">
-//   `;
+function mostrarProductos(productos){
+  let htmlContenedor = ''
+      productos.forEach(element => {
+      htmlContenedor += `
+      <div onclick='setProductID(${element.id})' class="list-group-item list-group-item-action cursor-active">
+          <div class="row">
+              <div class="col-3">
+                  <img src="${element.image}" alt="${element.description}" class="img-thumbnail">
+              </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      <h4 class="mb-1">${element.name}</h4>
+                      <small class="text-muted">${element.soldCount} artículos</small>
+                  </div>
+                  <p class="mb-1">${element.description}</p>
+              </div>
+          </div>
+      </div>
+      `  
+      })
+      return htmlContenedor  
+   }
 
-//   producto.images.forEach((element, index) => {
-//     const activeClass = index === 0 ? "active" : ""; // Agrega la clase 'active' al primer elemento
-//     contenedorImagenes += `
-//       <div class="carousel-item ${activeClass} text-center">
-//         <img src="${element}" class="d-block mx-auto custom-image" alt="Imagen ${
-//       index + 1}">
-//       </div>
-//     `;
-//   });
-
-//   contenedorImagenes += `
-      
-      
-//       <a class="carousel-control-prev" href="#carouselControls" role="button" data-bs-slide="prev">
-//       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//       <span class="visually-hidden">Anterior</span>
-//     </a>
-  
-//     <a class="carousel-control-next" href="#carouselControls" role="button" data-bs-slide="next">
-//       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//       <span class="visually-hidden">Siguiente</span>
-//     </a>
-
-//   `;
-
-//   return contenedorImagenes;
-// }
 
 function mostrarImagenes(producto) {
   let contenedorImagenes = `
@@ -116,6 +98,30 @@ function mostrarImagenes(producto) {
   `;
 
   return contenedorImagenes;
+}
+
+function mostrarProductoRelacionado(producto) {
+  let contenedorProductoRelacionado = '';
+  contenedorProductoRelacionado += '<div class="row">';
+  producto.relatedProducts.forEach(element => {
+    contenedorProductoRelacionado += `
+      <div class="col-md-3 mb-4">
+        <div class="card">
+          <img class="card-img-top" src="${element.image}" alt="Imagen">
+          <div class="card-body">
+            <p class="card-text">${element.name}</p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  contenedorProductoRelacionado += '</div>';
+  return contenedorProductoRelacionado;
+}
+
+function redireccionarProducto(id) { //Se declara funcion para guardar en localstorage el ID del producto
+  localStorage.setItem("IdProducto", id);
+  window.location = `product-info.html`;//Se redirige a la pagina product-info
 }
 
 
@@ -200,3 +206,15 @@ document
     mostrarComentarioNuevo(nuevoComentario);
     document.getElementById("envioComentario").reset();
   });
+
+
+
+
+
+
+  const productId = localStorage.getItem(ID);
+  const productoPrincipal = obtenerInformacionDelProducto(productId);
+  const productosRelacionadosIds = productoPrincipal.relatedProducts; 
+  const productosRelacionados = productos.filter((producto) => {
+    return productosRelacionadosIds.includes(producto.id);
+  })
